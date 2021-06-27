1ecc31f491a8458404abf2480ab45027b75159a4
Wanja Schulze <wangenau@protonmail.com>
2021-06-27 18:13:33 +0200

    init: Remove version from all so star imports work again
    * version = version to remove flake8 warning

--- plainedft/__init__.py
+++ plainedft/__init__.py
@@ -3,4 +3,5 @@ from .atoms import Atoms, load_atoms, read_xyz, save_atoms, write_cube
 from .scf import SCF
 from .version import __version__
 
-__all__ = ['Atoms', 'load_atoms', 'read_xyz', 'save_atoms', 'SCF', 'write_cube', __version__]
+__all__ = ['Atoms', 'load_atoms', 'read_xyz', 'save_atoms', 'SCF', 'write_cube']
+__version__ = __version__  # Supress flake8 warning
