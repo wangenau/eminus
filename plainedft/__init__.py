2e9e86f98a1584432840a87249c2314732ad7dc9
Wanja Schulze <wangenau@protonmail.com>
2021-06-27 16:07:45 +0200

    Add import/export functions to init

--- plainedft/__init__.py
+++ plainedft/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from .atoms import Atoms
+from .atoms import Atoms, load_atoms, read_xyz, save_atoms, write_cube
 from .scf import SCF
 from .version import __version__
 
-__all__ = ['Atoms', 'SCF', __version__]
+__all__ = ['Atoms', 'load_atoms', 'read_xyz', 'save_atoms', 'SCF', 'write_cube', __version__]
