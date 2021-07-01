13bc4b4004ae4932d68303e225d6cf32e70bc985
Wanja Schulze <wangenau@protonmail.com>
2021-07-01 14:35:46 +0200

    init: Add a minimal docstring with a minimal example

--- plainedft/__init__.py
+++ plainedft/__init__.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+'''
+PlaineDFT by wangenau
+
+Minimal usage example to do a DFT calculation for Hydrogen:
+$ from plainedft import *
+$ atoms = Atoms('H', [0, 0, 0])
+$ SCF(atoms)
+'''
 from .atoms import Atoms, load_atoms, read_xyz, save_atoms, write_cube
 from .scf import SCF
 from .version import __version__
