c4f4ab87636aaed647b8fd9a44e37b429d72a576
Wanja Schulze <wangenau@protonmail.com>
2021-09-19 13:52:41 +0200

    init: Make docstring a code block

--- plainedft/__init__.py
+++ plainedft/__init__.py
@@ -4,9 +4,11 @@ PlaineDFT - A simple plane wave DFT code.
 
 Minimal usage example to do a DFT calculation for Hydrogen:
 
-| from plainedft import *
-| atoms = Atoms('H', [0, 0, 0])
-| SCF(atoms)
+.. code-block:: bash
+
+    $ from plainedft import *
+    $ atoms = Atoms('H', [0, 0, 0])
+    $ SCF(atoms)
 '''
 from .atoms import Atoms, load_atoms, read_xyz, save_atoms, write_cube, write_xyz
 from .scf import SCF
