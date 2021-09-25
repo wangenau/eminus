97fa4fa557fa20ff691d36469dc30229e2322186
Wanja Schulze <wangenau@protonmail.com>
2021-09-25 11:32:54 +0200

    tests/spin_paired: Docstring, also compare all energies at once

--- tests/spin_paired/test_spin_paired.py
+++ tests/spin_paired/test_spin_paired.py
@@ -4,7 +4,9 @@ from plainedft import Atoms, SCF, read_xyz, __path__
 
 
 def test_spin_paired():
+    '''Compare total energies for small test-systems with reference values.'''
     # Total energies calculated with PWDFT.jl for H, H2, LiH, CH4, and Ne with same parameters
+    # These values can be generated with the file ref_spin_paired.jl
     Etot_ref = [-0.4353327, -1.10228799, -0.76526088, -7.71058153, -29.88936935]
 
     path = f'{__path__[0]}/../tests/spin_paired'
@@ -13,11 +15,15 @@ def test_spin_paired():
     ecut = 10
     S = 48
 
-    for i, sys in enumerate(systems):
+    Etot = []
+    for sys in systems:
         atom, X = read_xyz(f'{path}/{sys}.xyz')
         atoms = Atoms(atom=atom, X=X, a=a, ecut=ecut, S=S)
-        Etot = SCF(atoms)
-        assert_allclose(Etot, Etot_ref[i])
+        e = SCF(atoms)
+        Etot.append(e)
+
+    assert_allclose(Etot, Etot_ref)
+    return
 
 
 if __name__ == '__main__':
