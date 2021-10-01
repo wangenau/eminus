fe5ce7581894ed85cec0685ee8e4732e86fe3110
Wanja Schulze <wangenau@protonmail.com>
2021-10-01 22:19:59 +0200

    init: Add read_cube function

--- plainedft/__init__.py
+++ plainedft/__init__.py
@@ -10,9 +10,9 @@ Minimal usage example to do a DFT calculation for Hydrogen:
     atoms = Atoms('H', [0, 0, 0])
     SCF(atoms)
 '''
-from .atoms import Atoms, load_atoms, read_xyz, save_atoms, write_cube, write_xyz
+from .atoms import Atoms, load_atoms, read_cube, read_xyz, save_atoms, write_cube, write_xyz
 from .scf import SCF
 from .version import __version__
 
-__all__ = ['Atoms', 'load_atoms', 'read_xyz', 'save_atoms', 'SCF', 'write_cube', 'write_xyz',
-           '__version__']
+__all__ = ['Atoms', 'load_atoms', 'read_cube', 'read_xyz', 'save_atoms', 'SCF', 'write_cube',
+           'write_xyz', '__version__']
