f9df7e95ee762d0c81462cbeffa79597e8e0ec2d
Wanja Schulze <wangenau@protonmail.com>
2021-10-05 13:36:57 +0200

    version: Rename addon -> pkg

--- plainedft/version.py
+++ plainedft/version.py
@@ -2,7 +2,7 @@
 '''
 Package version number and version info function.
 '''
-import sys
+from sys import version
 
 __version__ = '0.0.1'
 
@@ -10,12 +10,12 @@ __version__ = '0.0.1'
 def info():
     '''Print version numbers and availability of used packages.'''
     print('--- Version infos ---')
-    print(f'python       : {sys.version.split()[0]}')
+    print(f'python       : {version.split()[0]}')
     print(f'plainedft    : {__version__}')
-    for addon in ('numpy', 'scipy', 'vispy', 'nglview', 'pyflosic_dev'):
+    for pkg in ('numpy', 'scipy', 'vispy', 'nglview', 'pyflosic_dev'):
         try:
-            exec(f'import {addon}')
-            print(f'{addon.ljust(13)}: {eval(addon).__version__}')
+            exec(f'import {pkg}')
+            print(f'{pkg.ljust(13)}: {eval(pkg).__version__}')
         except ModuleNotFoundError:
-            print(f'{addon.ljust(13)}: not installed')
+            print(f'{pkg.ljust(13)}: not installed')
     return
