250721bb6844a359f11196f85d198c8c04103d43
Wanja Schulze <wangenau@protonmail.com>
2021-10-06 13:48:27 +0200

    exc: Improve lda_pw_c_spin
    * Use sub-functions for fits, same for padefit

--- plainedft/exc.py
+++ plainedft/exc.py
@@ -132,7 +132,7 @@ def lda_pw_c(n):
     rs = pi34 / n**third
 
     a = 0.031091
-    a1 = 0.21370
+    a1 = 0.2137
     b1 = 7.5957
     b2 = 3.5876
     b3 = 1.6382
@@ -168,30 +168,6 @@ def lda_pw_c_spin(n, zeta):
     pi34 = (3 / (4 * np.pi))**(1 / 3)
     rs = pi34 / n**third
 
-    # Unpolarised parameters
-    a = 0.031091
-    a1 = 0.21370
-    b1 = 7.5957
-    b2 = 3.5876
-    b3 = 1.6382
-    b4 = 0.49294
-
-    # Polarised parameters
-    ap = 0.015545
-    a1p = 0.20548
-    b1p = 14.1189
-    b2p = 6.1977
-    b3p = 3.3662
-    b4p = 0.62517
-
-    # Antiferromagnetic parameters
-    aa = 0.016887
-    a1a = 0.11125
-    b1a = 10.357
-    b2a = 3.6231
-    b3a = 0.88026
-    b4a = 0.49671
-
     fz0 = 1.709921
 
     zeta2 = zeta * zeta
@@ -201,41 +177,48 @@ def lda_pw_c_spin(n, zeta):
     rs32 = rs * rs12
     rs2 = rs**2
 
-    # Unpolarised
-    om = 2 * a * (b1 * rs12 + b2 * rs + b3 * rs32 + b4 * rs2)
-    dom = 2 * a * (0.5 * b1 * rs12 + b2 * rs + 1.5 * b3 * rs32 + 2 * b4 * rs2)
-    olog = np.log(1 + 1 / om)
-    epwc = -2 * a * (1 + a1 * rs) * olog
-    vpwc = -2 * a * (1 + 2 / 3 * a1 * rs) * olog - 2 / 3 * a * (1 + a1 * rs) * dom / (om * (om + 1))
+    def pw_fit(i):
+        '''Calculate correlation energies by Perdew-Wang approximation interpolation.
 
-    # Polarized
-    omp = 2 * ap * (b1p * rs12 + b2p * rs + b3p * rs32 + b4p * rs2)
-    domp = 2 * ap * (0.5 * b1p * rs12 + b2p * rs + 1.5 * b3p * rs32 + 2 * b4p * rs2)
-    ologp = np.log(1 + 1 / omp)
-    epwcp = -2 * ap * (1 + a1p * rs) * ologp
-    vpwcp = -2 * ap * (1 + 2 / 3 * a1p * rs) * ologp - \
-             2 / 3 * ap * (1 + a1p * rs) * domp / (omp * (omp + 1))
+        Args:
+            i : int
+                Index weather to use unpolarised (0), polarised (1), or antiferromagnetic (2) fit.
 
-    # Antiferromagnetic
-    oma = 2 * aa * (b1a * rs12 + b2a * rs + b3a * rs32 + b4a * rs2)
-    doma = 2 * aa * (0.5 * b1a * rs12 + b2a * rs + 1.5 * b3a * rs32 + 2 * b4a * rs2)
-    ologa = np.log(1 + 1 / oma)
-    alpha = 2 * aa * (1 + a1a * rs) * ologa
-    vpwca = 2 * aa * (1 + 2 / 3 * a1a * rs) * ologa + \
-            2 / 3 * aa * (1 + a1a * rs) * doma / (oma * (oma + 1))
+        Returns:
+            PW fit and its derivative as a tuple(array, array).
+        '''
+        a = (0.031091, 0.015545, 0.016887)
+        a1 = (0.2137, 0.20548, 0.11125)
+        b1 = (7.5957, 14.1189, 10.357)
+        b2 = (3.5876, 6.1977, 3.6231)
+        b3 = (1.6382, 3.3662, 0.88026)
+        b4 = (0.49294, 0.62517, 0.49671)
+
+        om = 2 * a[i] * (b1[i] * rs12 + b2[i] * rs + b3[i] * rs32 + b4[i] * rs2)
+        dom = 2 * a[i] * (0.5 * b1[i] * rs12 + b2[i] * rs + 1.5 * b3[i] * rs32 + 2 * b4[i] * rs2)
+        olog = np.log(1 + 1 / om)
+
+        fit = -2 * a[i] * (1 + a1[i] * rs) * olog
+        dfit = -2 * a[i] * (1 + 2 / 3 * a1[i] * rs) * olog - \
+               2 / 3 * a[i] * (1 + a1[i] * rs) * dom / (om * (om + 1))
+        return fit, dfit
+
+    ecU, vcU = pw_fit(0)  # Unpolarised
+    ecP, vcP = pw_fit(1)  # Polarized
+    ac, dac = pw_fit(2)  # Spin stiffness
 
     fz = ((1 + zeta)**(4 / 3) + (1 - zeta)**(4 / 3) - 2) / (2**(4 / 3) - 2)
     dfz = ((1 + zeta)**(1 / 3) - (1 - zeta)**(1 / 3)) * 4 / (3 * (2**(4 / 3) - 2))
 
-    ec = epwc + alpha * fz * (1 - zeta4) / fz0 + (epwcp - epwc) * fz * zeta4
+    ec = ecU + ac * fz * (1 - zeta4) / fz0 + (ecP - ecU) * fz * zeta4
 
-    vcup = vpwc + vpwca * fz * (1 - zeta4) / fz0 + (vpwcp - vpwc) * fz * zeta4 + \
-           (alpha / fz0 * (dfz * (1 - zeta4) - 4 * fz * zeta3) +
-           (epwcp - epwc) * (dfz * zeta4 + 4 * fz * zeta3)) * (1 - zeta)
+    vcup = vcU + dac * fz * (1 - zeta4) / fz0 + (vcP - vcU) * fz * zeta4 + \
+           (ac / fz0 * (dfz * (1 - zeta4) - 4 * fz * zeta3) +
+           (ecP - ecU) * (dfz * zeta4 + 4 * fz * zeta3)) * (1 - zeta)
 
-    vcdw = vpwc + vpwca * fz * (1 - zeta4) / fz0 + (vpwcp - vpwc) * fz * zeta4 - \
-           (alpha / fz0 * (dfz * (1 - zeta4) - 4 * fz * zeta3) +
-           (epwcp - epwc) * (dfz * zeta4 + 4 * fz * zeta3)) * (1 + zeta)
+    vcdw = vcU + dac * fz * (1 - zeta4) / fz0 + (vcP - vcU) * fz * zeta4 - \
+           (ac / fz0 * (dfz * (1 - zeta4) - 4 * fz * zeta3) +
+           (ecP - ecU) * (dfz * zeta4 + 4 * fz * zeta3)) * (1 + zeta)
     return ec, [vcup, vcdw]
 
 
@@ -307,9 +290,43 @@ def lda_vwn_c_spin(n, zeta):
     fz = cfz1 * (trup13 * trup + trdw13 * trdw - 2)  # f(zeta)
     dfz = cfz2 * (trup13 - trdw13)  # df/dzeta
 
-    ecP, vcP = padefit(sqrtrs, 0)  # Paramagnetic fit
-    ecF, vcF = padefit(sqrtrs, 1)  # Ferromagnetic fit
-    ac, dac = padefit(sqrtrs, 2)  # ac = "spin stiffness"
+    def pade_fit(i):
+        '''Calculate correlation energies by Pade approximation interpolation.
+
+        Args:
+            i : int
+                Index weather to use paramagnetic (0), ferromagnetic (1), or spin stiffness (2) fit.
+
+        Returns:
+            Pade fit and its derivative as a tuple(array, array).
+        '''
+        A = (0.0310907, 0.01554535, -0.01688686394039)
+        b = (3.72744, 7.06042, 1.13107)
+        c = (12.9352, 18.0578, 13.0045)
+        x0 = (-0.10498, -0.325, -0.0047584)
+        Q = (6.15199081975908, 4.73092690956011, 7.12310891781812)
+        tbQ = (1.21178334272806, 2.98479352354082, 0.31757762321188)
+        bx0fx0 = (-0.03116760867894, -0.14460061018521, -0.00041403379428)
+
+        xx0 = sqrtrs - x0[i]
+        Qtxb = Q[i] / (2 * sqrtrs + b[i])
+        atg = np.arctan(Qtxb)
+        fx = rs + b[i] * sqrtrs + c[i]
+
+        fit = A[i] * (np.log(rs / fx) + tbQ[i] * atg -
+              bx0fx0[i] * (np.log(xx0 * xx0 / fx) + (tbQ[i] + 4 * x0[i] / Q[i]) * atg))
+
+        txb = 2 * sqrtrs + b[i]
+        txbfx = txb / fx
+        itxbQ = 1 / (txb * txb + Q[i] * Q[i])
+
+        dfit = fit - A[i] / 3 + A[i] * sqrtrs / 6 * (txbfx + 4 * b[i] * itxbQ +
+               bx0fx0[i] * (2 / xx0 - txbfx - 4 * (b[i] + 2 * x0[i]) * itxbQ))
+        return fit, dfit
+
+    ecP, vcP = pade_fit(0)  # Paramagnetic fit
+    ecF, vcF = pade_fit(1)  # Ferromagnetic fit
+    ac, dac = pade_fit(2)  # Spin stiffness
 
     ac = ac * iddfz0
     dac = dac * iddfz0
@@ -323,45 +340,3 @@ def lda_vwn_c_spin(n, zeta):
     vcup = dec1 + (1 - zeta) * dec2
     vcdw = dec1 - (1 + zeta) * dec2
     return ec, [vcup, vcdw]
-
-
-# Adapted from https://github.com/f-fathurrahman/PWDFT.jl/blob/master/src/XC_funcs/XC_c_vwn_spin.jl
-def padefit(x, i):
-    '''Calculate correlation energies by Pade approximation interpolation.
-
-    Args:
-        x : array
-              Square root of the density.
-
-        i : int
-            Index weather to use a paramagnetic (0), ferromagnetic (1), or spin stiffness (2) fit.
-
-    Returns:
-        Pade fit and its derivative as a tuple(array, array).
-    '''
-    A = (0.0310907, 0.01554535, -0.01688686394039)
-    b = (3.72744, 7.06042, 1.13107)
-    c = (12.9352, 18.0578, 13.0045)
-    x0 = (-0.10498, -0.32500, -0.0047584)
-    Q = (6.15199081975908, 4.73092690956011, 7.12310891781812)
-    tbQ = (1.21178334272806, 2.98479352354082, 0.31757762321188)
-    bx0fx0 = (-0.03116760867894, -0.14460061018521, -0.00041403379428)
-
-    # Pade fit calculated in x and its derivative with respect to rho
-    # rs = inv((rho*)^(1/3)) = x^2
-    sqx = x * x                   # x^2 = r_s
-    xx0 = x - x0[i]               # x - x_0
-    Qtxb = Q[i] / (2 * x + b[i])  # Q / (2x+b)
-    atg = np.arctan(Qtxb)         # tan^-1(Q/(2x+b))
-    fx = sqx + b[i] * x + c[i]    # X(x) = x^2 + b*x + c
-
-    fit = A[i] * (np.log(sqx / fx) + tbQ[i] * atg -
-          bx0fx0[i] * (np.log(xx0 * xx0 / fx) + (tbQ[i] + 4 * x0[i] / Q[i]) * atg))
-
-    txb = 2 * x + b[i]
-    txbfx = txb / fx
-    itxbQ = 1 / (txb * txb + Q[i] * Q[i])
-
-    dfit = fit - A[i] / 3 + A[i] * x / 6 * (txbfx + 4 * b[i] * itxbQ +
-           bx0fx0[i] * (2 / xx0 - txbfx - 4 * (b[i] + 2 * x0[i]) * itxbQ))
-    return fit, dfit
