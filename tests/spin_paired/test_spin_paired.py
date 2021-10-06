63139a5081a3949b9c80e2c3e20e832f8182175a
Wanja Schulze <wangenau@protonmail.com>
2021-10-06 15:40:43 +0200

    tests: Split spinpol test into single calculations
    * User sees progress this way

--- tests/spin_paired/test_spin_paired.py
+++ tests/spin_paired/test_spin_paired.py
@@ -2,35 +2,56 @@ from numpy.testing import assert_allclose
 
 from plainedft import Atoms, SCF, read_xyz, __path__
 
+# Total energies calculated with PWDFT.jl for H, H2, LiH, CH4, and Ne with same parameters as below
+# These values can be generated with the file ref_spin_paired.jl
+Etot_ref = [-0.4353327, -1.10228799, -0.76526088, -7.71058153, -29.88936935]
 
-def test_spin_paired():
-    '''Compare total energies for small test-systems with reference values.'''
-    # Total energies calculated with PWDFT.jl for H, H2, LiH, CH4, and Ne with same parameters
-    # These values can be generated with the file ref_spin_paired.jl
-    Etot_ref = [-0.4353327, -1.10228799, -0.76526088, -7.71058153, -29.88936935]
 
+def calc_spin_paired(system, E_ref):
+    '''Compare total energies for a test system with a reference value.'''
     path = f'{__path__[0]}/../tests/spin_paired'
-    systems = ['H', 'H2', 'LiH', 'CH4', 'Ne']
     a = 16
     ecut = 10
     S = 48
+    verbose = 0
 
-    Etot = []
-    for sys in systems:
-        atom, X = read_xyz(f'{path}/{sys}.xyz')
-        atoms = Atoms(atom=atom, X=X, a=a, ecut=ecut, S=S)
-        e = SCF(atoms)
-        Etot.append(e)
+    atom, X = read_xyz(f'{path}/{system}.xyz')
+    atoms = Atoms(atom=atom, X=X, a=a, ecut=ecut, S=S, verbose=verbose)
+    E = SCF(atoms)
 
     try:
-        assert_allclose(Etot, Etot_ref)
+        assert_allclose(E, E_ref)
     except AssertionError as err:
-        print('Test failed.')
-        raise SystemExit(err)
+        print(f'Test for {system} failed.')
+        raise SystemExit(err) from None
     else:
-        print('Test passed.')
+        print(f'Test for {system} passed.')
     return
 
 
+def test_H():
+    calc_spin_paired('H', Etot_ref[0])
+
+
+def test_H2():
+    calc_spin_paired('H2', Etot_ref[1])
+
+
+def test_LiH():
+    calc_spin_paired('LiH', Etot_ref[2])
+
+
+def test_CH4():
+    calc_spin_paired('CH4', Etot_ref[3])
+
+
+def test_Ne():
+    calc_spin_paired('Ne', Etot_ref[4])
+
+
 if __name__ == '__main__':
-    test_spin_paired()
+    test_H()
+    test_H2()
+    test_LiH()
+    test_CH4()
+    test_Ne()
