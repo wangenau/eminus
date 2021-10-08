f2626d4092d49a7ec75bedb8b9c9bb48d271436d
Wanja Schulze <wangenau@protonmail.com>
2021-10-08 12:00:08 +0200

    init: Expose get_psi and get_epsilon

--- plainedft/__init__.py
+++ plainedft/__init__.py
@@ -10,8 +10,8 @@ Minimal usage example to do a DFT calculation for Hydrogen::
 '''
 from .atoms import Atoms
 from .atoms_io import load_atoms, read_cube, read_xyz, save_atoms, write_cube, write_xyz
-from .scf import SCF
+from .scf import get_epsilon, get_psi, SCF
 from .version import info, __version__
 
-__all__ = ['Atoms', 'info', 'load_atoms', 'read_cube', 'read_xyz', 'save_atoms', 'SCF',
-           'write_cube', 'write_xyz', '__version__']
+__all__ = ['Atoms', 'get_epsilon', 'get_psi', 'info', 'load_atoms', 'read_cube', 'read_xyz',
+           'save_atoms', 'SCF', 'write_cube', 'write_xyz', '__version__']
