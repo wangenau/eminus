d566448f79efd6c729b1e680d916d2b9162545ab
Wanja Schulze <wangenau@protonmail.com>
2021-11-23 13:36:19 +0100

    version: Release v1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## v1.0.1
+
+* Add branding
+* Fix gth files not included in pypi build
+
+## v1.0.0
+
+* Initial release
--- eminus/version.py
+++ eminus/version.py
@@ -4,7 +4,7 @@ Package version number and version info function.
 '''
 from sys import version
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 def info():
