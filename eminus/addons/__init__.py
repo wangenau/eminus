786d9e2c87c775321da2b9a637298d7c158d76e7
Wanja Schulze <wangenau@protonmail.com>
2021-11-24 15:35:26 +0100

    addons/init: Correct installation hint

--- eminus/addons/__init__.py
+++ eminus/addons/__init__.py
@@ -4,7 +4,7 @@ Addon functions that need additional dependencies to work.
 
 To also install all dependecies to use them, use::
 
-    pip install .[addons]
+    pip install eminus[addons]
 '''
 from .fods import get_fods, remove_core_fods, split_atom_and_fod
 from .viewer import view_grid, view_mol
