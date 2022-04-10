6eb60ddb32d42ff839887de5ded5dc047acb41b4
Wanja Schulze <wangenau@protonmail.com>
2022-04-10 15:39:20 +0200

    version: Split dependencies and addons

--- eminus/version.py
+++ eminus/version.py
@@ -5,6 +5,8 @@ Package version number and version info function.
 from sys import version
 
 __version__ = '1.0.1'
+dependencies = ('numpy', 'scipy')
+addons = ('notebook', 'vispy', 'nglview', 'jupyter_rfb', 'pyflosic_dev')
 
 
 def info():
@@ -12,10 +14,13 @@ def info():
     print('--- Version infos ---')
     print(f'python       : {version.split()[0]}')
     print(f'eminus       : {__version__}')
-    for pkg in ('numpy', 'scipy', 'notebook', 'vispy', 'nglview', 'jupyter_rfb', 'pyflosic_dev'):
+    for pkg in dependencies + addons:
         try:
             exec(f'import {pkg}')
             print(f'{pkg.ljust(13)}: {eval(pkg).__version__}')
         except ModuleNotFoundError:
-            print(f'{pkg.ljust(13)}: not installed')
+            if pkg in dependencies:
+                print(f'{pkg.ljust(13)}: Dependency not installed')
+            elif pkg in addons:
+                print(f'{pkg.ljust(13)}: Addon not installed')
     return
