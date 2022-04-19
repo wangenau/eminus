a833629f060db5da2f169d7dd36ca437f503ce74
Wanja Schulze <wangenau@protonmail.com>
2022-04-19 23:01:36 +0200

    potentials: Improve and document

--- eminus/potentials.py
+++ eminus/potentials.py
@@ -11,11 +11,11 @@ def Harmonic(atoms):
         atoms: Atoms object.
 
     Returns:
-        array: Harmonical potential.
+        array: Harmonical potential in reciprocal space.
     '''
     dr = norm(atoms.r - np.sum(atoms.R, axis=1) / 2, axis=1)
-    V = 2 * dr**2
-    return atoms.Jdag(atoms.O(atoms.J(V)))
+    Vharm = 2 * dr**2
+    return atoms.Jdag(atoms.O(atoms.J(Vharm)))
 
 
 def Coulomb(atoms):
@@ -25,40 +25,45 @@ def Coulomb(atoms):
         atoms: Atoms object.
 
     Returns:
-        array: Coulomb potential.
+        array: Coulomb potential in reciprocal space.
     '''
     Z = atoms.Z[0]  # Potential should only be used for same species
-    Vcoul = -4 * np.pi * Z / atoms.G2[1:]
-    Vcoul = np.concatenate(([0], Vcoul))
+    Vcoul = np.empty_like(atoms.G2)
+    Vcoul[0] = 0
+    Vcoul[1:] = -4 * np.pi * Z / atoms.G2[1:]
 
     Sf = np.sum(atoms.Sf, axis=0)
     return atoms.J(Vcoul * Sf)
 
 
 def Ge(atoms):
-    '''Starkloff-Joannopoulos pseudopotential for Germanium. Fourier-transformed by Tomas Arias.
+    '''Starkloff-Joannopoulos local pseudopotential for germanium.
+
+    Fourier-transformed by Tomas Arias.
+    Reference: Phys. Rev. B 16, 5212.
 
     Args:
         atoms: Atoms object.
 
     Returns:
-        array: Germanium pseudopotential.
+        array: Germanium pseudopotential in reciprocal space.
     '''
-    Z = atoms.Z[0]  # Potential should only be used for same species/Germanium
+    Z = 4  # Potential should only be used for germanium
     lamda = 18.5
     rc = 1.052
     Gm = np.sqrt(atoms.G2[1:])
+    Vps = np.empty_like(atoms.G2)
 
-    Vps = -2 * np.pi * np.exp(-np.pi * Gm / lamda) * np.cos(rc * Gm) * (Gm / lamda) / \
-          (1 - np.exp(-2 * np.pi * Gm / lamda))
-    for n in range(5):
-        Vps += (-1)**n * np.exp(-lamda * rc * n) / (1 + (n * lamda / Gm)**2)
-    Vps = Vps * 4 * np.pi * Z / Gm**2 * (1 + np.exp(-lamda * rc)) - 4 * np.pi * Z / Gm**2
-
+    # Special case for G=(0,0,0)
     n = np.arange(1, 5)
-    eps = 4 * np.pi * Z * (1 + np.exp(-lamda * rc)) * (rc**2 / 2 + 1 / lamda**2 * (np.pi**2 / 6 +
-          np.sum((-1)**n * np.exp(-lamda * rc * n) / n**2)))
-    Vps = np.concatenate(([eps], Vps))
+    Vps[0] = 4 * np.pi * Z * (1 + np.exp(-lamda * rc)) * (rc**2 / 2 + 1 / lamda**2 * (np.pi**2 / 6 +
+             np.sum((-1)**n * np.exp(-lamda * rc * n) / n**2)))
+
+    Vps[1:] = -2 * np.pi * np.exp(-np.pi * Gm / lamda) * np.cos(rc * Gm) * (Gm / lamda) / \
+              (1 - np.exp(-2 * np.pi * Gm / lamda))
+    for n in range(5):
+        Vps[1:] = Vps[1:] + (-1)**n * np.exp(-lamda * rc * n) / (1 + (n * lamda / Gm)**2)
+    Vps[1:] = Vps[1:] * 4 * np.pi * Z / Gm**2 * (1 + np.exp(-lamda * rc)) - 4 * np.pi * Z / Gm**2
 
     Sf = np.sum(atoms.Sf, axis=0)
     return atoms.J(Vps * Sf)
@@ -71,7 +76,7 @@ def init_pot(atoms):
         atoms: Atoms object.
 
     Returns:
-        array: Potential.
+        array: Potential in reciprocal space.
     '''
     implemented = {'harmonic': Harmonic, 'coulomb': Coulomb, 'ge': Ge}
     try:
