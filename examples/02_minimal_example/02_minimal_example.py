5a255adde28387b0c98f69f08ed540ce97868387
Wanja Schulze <wangenau@protonmail.com>
2022-09-19 14:32:49 +0200

    examples: Regen notebooks and add nspin to example 02

--- examples/02_minimal_example/02_minimal_example.py
+++ examples/02_minimal_example/02_minimal_example.py
@@ -1,7 +1,7 @@
 from eminus import Atoms, SCF
 
 # # Create an `Atoms` object with helium at position (0,0,0)
-atoms = Atoms('He', [0, 0, 0])
+atoms = Atoms('He', [0, 0, 0], Nspin=1)
 
 # # Create a `SCF` object...
 scf = SCF(atoms)
