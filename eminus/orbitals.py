de9d3bec66ad8294c2d00ec1b9b923e81252ec32
Wanja Schulze <wangenau@protonmail.com>
2023-02-01 16:02:19 +0100

    orbitals: Sort atom types when writing cubes

--- eminus/orbitals.py
+++ eminus/orbitals.py
@@ -103,7 +103,7 @@ def cube_writer(atoms, type, orbitals):
     '''
     # Create the system name
     name = ''
-    for ia in set(atoms.atom):
+    for ia in set(sorted(atoms.atom)):
         # Skip the number of atoms if it is equal to one
         na = atoms.atom.count(ia)
         name += f'{ia}{na if na > 1 else ""}'
