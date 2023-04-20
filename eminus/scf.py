f13f64f316ceeb253c6456e0183e3adfe0a52007
Wanja Schulze <wangenau@protonmail.com>
2023-04-20 16:17:07 +0200

    scf: debug

--- eminus/scf.py
+++ eminus/scf.py
@@ -201,6 +201,7 @@ class SCF:
         '''
         # Get the COM before centering the atoms
         com = center_of_mass(self.atoms.X)
+        print(com)
 
         # Run the recenter method of the atoms object
         self.atoms.recenter(center=center)
@@ -210,6 +211,8 @@ class SCF:
         else:
             center = np.asarray(center)
             dr = com - center
+
+        print(dr)
 
         # Shift orbitals and density
         self.W = self.atoms.T(self.W, dr=-dr)
