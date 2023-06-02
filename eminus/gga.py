22a556f56baa680b5ce7e52381a8d775e942d220
Wanja Schulze <wangenau@protonmail.com>
2023-06-02 14:08:11 +0200

    gga: Move get_tau down

--- eminus/gga.py
+++ eminus/gga.py
@@ -26,6 +26,40 @@ def get_grad_field(atoms, field, real=True):
     return dfield
 
 
+def gradient_correction(atoms, spin, dn_spin, vsigma):
+    '''Calculate the gradient correction for the exchange-correlation potential.
+
+    Reference: Chem. Phys. Lett. 199, 557.
+
+    Args:
+        atoms: Atoms object.
+        spin (int): Spin variable to track weather to calculate the gradient for spin up or down.
+        dn_spin (ndarray): Real-space gradient of densities per spin channel.
+        vsigma (ndarray): Contracted gradient potential derivative.
+
+    Returns:
+        ndarray: Gradient correction in reciprocal space.
+    '''
+    # sigma is |dn|^2, while vsigma is n * d exc/d sigma
+    h = np.zeros_like(dn_spin)
+    if atoms.Nspin == 1:
+        # In the unpolarized case we have no spin mixing and only one spin density
+        h[0] = 2 * vsigma[0, :, None] * dn_spin[0]
+    else:
+        # In the polarized case we would get for spin up (and similar for spin down)
+        # Vxc_u = vxc_u - Nabla dot (2 vsigma_uu * dn_u + vsigma_ud * dn_d)
+        # h is the expression in the brackets
+        h[0] = 2 * vsigma[0, :, None] * dn_spin[0] + vsigma[1, :, None] * dn_spin[1]
+        h[1] = 2 * vsigma[2, :, None] * dn_spin[1] + vsigma[1, :, None] * dn_spin[0]
+
+    # Calculate Nabla dot h
+    # Normally we would calculate the correction for each spin, but we only need one at a time in H
+    Gh = np.empty((len(atoms.G2), 3), dtype=complex)
+    for dim in range(3):
+        Gh[:, dim] = atoms.J(h[spin, :, dim])
+    return 1j * np.sum(atoms.G * Gh, axis=1)
+
+
 def get_tau(atoms, Y):
     '''Calculate the positive-definite kinetic energy densities per spin.
 
@@ -60,40 +94,6 @@ def get_tau(atoms, Y):
     # Use the definition with a division by two
     tau = 0.5 * np.sum(atoms.f[:, None, :] * sumdYrs, axis=2)
     return tau
-
-
-def gradient_correction(atoms, spin, dn_spin, vsigma):
-    '''Calculate the gradient correction for the exchange-correlation potential.
-
-    Reference: Chem. Phys. Lett. 199, 557.
-
-    Args:
-        atoms: Atoms object.
-        spin (int): Spin variable to track weather to calculate the gradient for spin up or down.
-        dn_spin (ndarray): Real-space gradient of densities per spin channel.
-        vsigma (ndarray): Contracted gradient potential derivative.
-
-    Returns:
-        ndarray: Gradient correction in reciprocal space.
-    '''
-    # sigma is |dn|^2, while vsigma is n * d exc/d sigma
-    h = np.zeros_like(dn_spin)
-    if atoms.Nspin == 1:
-        # In the unpolarized case we have no spin mixing and only one spin density
-        h[0] = 2 * vsigma[0, :, None] * dn_spin[0]
-    else:
-        # In the polarized case we would get for spin up (and similar for spin down)
-        # Vxc_u = vxc_u - Nabla dot (2 vsigma_uu * dn_u + vsigma_ud * dn_d)
-        # h is the expression in the brackets
-        h[0] = 2 * vsigma[0, :, None] * dn_spin[0] + vsigma[1, :, None] * dn_spin[1]
-        h[1] = 2 * vsigma[2, :, None] * dn_spin[1] + vsigma[1, :, None] * dn_spin[0]
-
-    # Calculate Nabla dot h
-    # Normally we would calculate the correction for each spin, but we only need one at a time in H
-    Gh = np.empty((len(atoms.G2), 3), dtype=complex)
-    for dim in range(3):
-        Gh[:, dim] = atoms.J(h[spin, :, dim])
-    return 1j * np.sum(atoms.G * Gh, axis=1)
 
 
 def calc_Vtau(scf, spin, W, vtau):
