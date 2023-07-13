329663b08937911bed8ab0cdaf147cbdeb2c5dfe
Wanja Schulze <wangenau@protonmail.com>
2023-07-13 16:06:00 +0200

    examples: Fix for mypy in sic example

--- examples/09_sic_calculations/09_sic_calculations.py
+++ examples/09_sic_calculations/09_sic_calculations.py
@@ -8,14 +8,14 @@ scf = SCF(atoms, sic=True)
 scf.run()
 
 # # Generate Kohn-Sham and Fermi-Loewdin orbitals
-FLO = FLO(scf)
+flo = FLO(scf)
 
 # # Print the self-interaction energy from the `SCF` object
 print(f'\nKSO-SIC energy = {scf.energies.Esic} Eh')
 
 # # The quality of the FLO-SIC energy will vary with the FOD guess
 # # The one-shot FLO-SIC energy should be lower than the KSO-SIC one
-esic = get_Esic(scf, atoms.J(FLO, False))
+esic = get_Esic(scf, atoms.J(flo, full=False))
 print(f'FLO-SIC energy = {esic} Eh')
 # # The SIC energy will also be saved in the `SCF` object
 print(f'\nAll energies:\n{scf.energies}')
