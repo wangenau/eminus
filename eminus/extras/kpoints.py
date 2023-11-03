36137a90dcefdd054f6e0a10d2015f5b1117be4d
Wanja Schulze <wangenau@protonmail.com>
2023-11-03 16:28:07 +0100

    extras/kpoints: Remove wrong conversion

--- eminus/extras/kpoints.py
+++ eminus/extras/kpoints.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Symmetrize k-points."""
 
-from ..kpoints import kpoint_convert
 from ..logger import log
 
 
@@ -29,11 +28,11 @@ def symmetrize(atoms, space_group_symmetry=True, time_reversal_symmetry=False):
     cell.atom = [[atoms.atom[ia], atoms.pos[ia]] for ia in range(atoms.Natoms)]
     cell.a = atoms.a
     cell.build()
-    kpts = KPoints(cell, atoms.kpts.k_scaled)
+    kpts = KPoints(cell, atoms.kpts.k)
     kpts.build(space_group_symmetry=space_group_symmetry,
                time_reversal_symmetry=time_reversal_symmetry)
 
-    atoms.kpts._k_scaled = kpts.kpts_ibz
-    atoms.kpts.k = kpoint_convert(atoms.kpts.k_scaled + atoms.kpts.kshift, atoms.a)
+    atoms.kpts.k = kpts.kpts_ibz
     atoms.kpts.wk = kpts.weights_ibz
+    atoms.kpts._k_scaled = None
     atoms.kpts.is_built = True
