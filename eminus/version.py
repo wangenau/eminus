c0854363a4e470010905be956fc84aef26a96866
Wanja Schulze <wangenau@protonmail.com>
2023-11-06 10:40:20 +0100

    version: Add missing extras to info

--- eminus/version.py
+++ eminus/version.py
@@ -14,29 +14,29 @@ LOGO = (' ___ _____ _ ___ _ _ ___ \n'
 def info():
     """Print version numbers and availability of packages."""
     dependencies = ('numpy', 'scipy')
-    extras = ('torch', 'pyscf', 'plotly', 'nglview')
+    extras = ('torch', 'pyscf', 'dftd3', 'plotly', 'nglview', 'matplotlib')
     dev = ('notebook', 'pylibxc', 'pytest', 'sphinx', 'furo')
 
     print(LOGO)
     print('--- Platform infos ---'
-          f'\nPlatform : {platform.system()} {platform.machine()}'
-          f'\nRelease  : {platform.release()} {platform.version()}'
+          f'\nPlatform   : {platform.system()} {platform.machine()}'
+          f'\nRelease    : {platform.release()} {platform.version()}'
           '\n\n--- Version infos ---'
-          f'\npython   : {sys.version.split()[0]}'
-          f'\neminus   : {__version__}')
+          f'\npython     : {sys.version.split()[0]}'
+          f'\neminus     : {__version__}')
     for pkg in dependencies + extras + dev:
         try:
             module = importlib.import_module(pkg)
             try:
-                print(f'{pkg:<9}: {module.__version__}')
+                print(f'{pkg:<11}: {module.__version__}')
             except AttributeError:
                 # pylibxc does not use the standard version identifier
-                print(f'{pkg:<9}: {module.version.__version__}')
+                print(f'{pkg:<11}: {module.version.__version__}')
         except ModuleNotFoundError:  # noqa: PERF203
             if pkg in dependencies:
-                print(f'{pkg:<9}: Dependency not installed')
+                print(f'{pkg:<11}: Dependency not installed')
             elif pkg in extras:
-                print(f'{pkg:<9}: Extra not installed')
+                print(f'{pkg:<11}: Extra not installed')
 
 
 if __name__ == '__main__':
