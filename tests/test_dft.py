2854e4b32a272c7ddfcfbbc9efacd03370f3ee47
Wanja Schulze <wangenau@protonmail.com>
2024-01-11 16:20:59 +0100

    tests: Add dft tests for k-points and unocc. states

--- tests/test_dft.py
+++ tests/test_dft.py
@@ -4,16 +4,17 @@ import numpy as np
 from numpy.testing import assert_allclose
 import pytest
 
-from eminus import Atoms, demo, SCF
+from eminus import Atoms, Cell, demo, SCF
 from eminus.dft import get_n_single, get_n_spin, get_n_total, get_psi, guess_pseudo, guess_random, H
 
-atoms_unpol = Atoms('Ne', (0, 0, 0), ecut=1, unrestricted=False)
+atoms_unpol = Cell('Si', 'diamond', 1, 10, kmesh=(2, 1, 1), bands=8)
 atoms_pol = Atoms('Ne', (0, 0, 0), ecut=1, unrestricted=True)
 
 scf_unpol = SCF(atoms_unpol)
-scf_unpol.run()
+scf_unpol.run(betat=1e-4)
+scf_unpol.converge_bands()
 scf_pol = SCF(atoms_pol)
-scf_pol.run()
+scf_pol.run(betat=1e-4)
 
 
 @pytest.mark.parametrize('guess', ['random', 'pseudo'])
@@ -29,13 +30,25 @@ def test_wavefunction(guess, unrestricted):
     elif guess == 'pseudo':
         W = guess_pseudo(scf)
     psi = get_psi(scf, W)
-    for s in range(scf.atoms.occ.Nspin):
-        # Test that WOW is the identity
-        ovlp = W[0][s].conj().T @ scf.atoms.O(W[0][s])
-        assert_allclose(np.diag(ovlp), np.ones(scf.atoms.occ.Nstate))
-        # Also test that psiOpsi is the identity
-        ovlp = psi[0][s].conj().T @ scf.atoms.O(psi[0][s])
-        assert_allclose(np.diag(ovlp), np.ones(scf.atoms.occ.Nstate))
+    for ik in range(scf.kpts.Nk):
+        for s in range(scf.atoms.occ.Nspin):
+            # Test that WOW is the identity
+            ovlp = W[ik][s].conj().T @ scf.atoms.O(W[ik][s])
+            assert_allclose(np.diag(ovlp), np.ones(scf.atoms.occ.Nstate))
+            # Also test that psiOpsi is the identity
+            ovlp = psi[ik][s].conj().T @ scf.atoms.O(psi[ik][s])
+            assert_allclose(np.diag(ovlp), np.ones(scf.atoms.occ.Nstate))
+
+
+def test_unocc_wavefunction():
+    """Test the orthonormalization of unoccupied wave functions."""
+    for ik in range(scf_unpol.kpts.Nk):
+        for s in range(scf_unpol.atoms.occ.Nspin):
+            # Test that DOD is the identity
+            ovlp = scf_unpol.D[ik][s].conj().T @ scf_unpol.atoms.O(scf_unpol.D[ik][s])
+            assert_allclose(np.diag(ovlp), np.ones(scf_unpol.atoms.occ.Nempty))
+            # Test that the unoccupied wavefunctions are orthogonal to the occupied ones
+            assert_allclose(scf_unpol.D[ik][s].conj().T @ scf_unpol.Y[ik][s], 0, atol=1e-15)
 
 
 @pytest.mark.parametrize('unrestricted', [True, False])
@@ -46,11 +59,12 @@ def test_H(unrestricted):
     else:
         scf = scf_unpol
     psi = get_psi(scf, scf.W)
-    for s in range(scf.atoms.occ.Nspin):
-        HW = H(scf, 0, s, psi)
-        WHW = psi[0][s].conj().T @ HW
-        # Test that WHW is a diagonal matrix
-        assert_allclose(WHW, np.diag(np.diag(WHW)), atol=1e-12)
+    for ik in range(scf.kpts.Nk):
+        for s in range(scf.atoms.occ.Nspin):
+            HW = H(scf, ik, s, psi)
+            WHW = psi[ik][s].conj().T @ HW
+            # Test that WHW is a diagonal matrix
+            assert_allclose(WHW, np.diag(np.diag(WHW)), atol=1e-12)
 
 
 @pytest.mark.parametrize('unrestricted', [True, False])
@@ -63,7 +77,7 @@ def test_n_total(unrestricted):
     # Test that the integrated density gives the number of electrons
     n = get_n_total(scf.atoms, scf.Y)
     n_int = np.sum(n) * scf.atoms.dV
-    assert_allclose(n_int, np.sum(scf.atoms.occ.f))
+    assert_allclose(n_int, np.sum(scf.atoms.occ.f * scf.kpts.wk[:, None, None]))
 
 
 @pytest.mark.parametrize('unrestricted', [True, False])
@@ -75,9 +89,10 @@ def test_n_spin(unrestricted):
         scf = scf_unpol
     # Test that the integrated spin densities gives the number of electrons per spin
     n = get_n_spin(scf.atoms, scf.Y)
-    for s in range(scf.atoms.occ.Nspin):
-        n_int = np.sum(n[s]) * scf.atoms.dV
-        assert_allclose(n_int, np.sum(scf.atoms.occ.f[0, s]))
+    for ik in range(scf.kpts.Nk):
+        for s in range(scf.atoms.occ.Nspin):
+            n_int = np.sum(n[s]) * scf.atoms.dV
+            assert_allclose(n_int, np.sum(scf.atoms.occ.f[ik, s]))
 
 
 @pytest.mark.parametrize('unrestricted', [True, False])
@@ -89,9 +104,10 @@ def test_n_single(unrestricted):
         scf = scf_unpol
     # Test that the integrated single orbital densities gives occupation number per orbital
     n = get_n_single(scf.atoms, scf.Y)
-    for s in range(scf.atoms.occ.Nspin):
-        n_int = np.sum(n[s], axis=0) * scf.atoms.dV
-        assert_allclose(n_int, scf.atoms.occ.f[0, s])
+    for ik in range(scf.kpts.Nk):
+        for s in range(scf.atoms.occ.Nspin):
+            n_int = np.sum(n[s], axis=0) * scf.atoms.dV
+            assert_allclose(n_int, scf.atoms.occ.f[ik, s])
 
 
 def test_demo():
