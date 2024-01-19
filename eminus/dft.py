Fix eigenenergy calculation of unoccupied states, add band structure tests, small restructure

eminus/dft.py
@@ -293,7 +293,7 @@ def get_psi(scf, W, **kwargs):
 
 
 def get_epsilon(scf, W, **kwargs):
-    """Calculate eigenvalues from H.
+    """Calculate eigenvalues from H of unoccupied states.
 
     Reference: Comput. Phys. Commun. 128, 1.
 
@@ -317,6 +317,33 @@ def get_epsilon(scf, W, **kwargs):
     return epsilon
 
 
+def get_epsilon_unocc(scf, W, Z, **kwargs):
+    """Calculate eigenvalues from H.
+
+    Reference: Comput. Phys. Commun. 128, 1.
+
+    Args:
+        scf: SCF object.
+        W (ndarray): Expansion coefficients of unconstrained wave functions in reciprocal space.
+        Z (ndarray): Expansion coefficients of unconstrained wave functions in reciprocal space.
+
+    Keyword Args:
+        **kwargs: See :func:`H`.
+
+    Returns:
+        ndarray: Eigenvalues.
+    """
+    atoms = scf.atoms
+    Y = orth(atoms, W)
+    D = orth_unocc(atoms, Y, Z)
+    epsilon = np.empty((atoms.kpts.Nk, atoms.occ.Nspin, D[0].shape[-1]))
+    for ik in range(atoms.kpts.Nk):
+        for spin in range(atoms.occ.Nspin):
+            mu = D[ik][spin].conj().T @ H(scf, ik, spin, D, **kwargs)
+            epsilon[ik][spin] = np.sort(eigvalsh(mu))
+    return epsilon
+
+
 def get_epsilon_band(scf):
     """Calculate eigenvalues from H for all bands, independent of occupations.
 
@@ -334,7 +361,7 @@ def get_epsilon_band(scf):
     # Calculate the unoccupied band energies, use the maximum number of empty states
     Nocc_k = np.sum(scf.atoms.occ.f > 0, axis=2)
     scf.converge_empty_bands(Nempty=np.max(Nempty_k))
-    e_unocc = get_epsilon(scf, scf.Z, **scf._precomputed)
+    e_unocc = get_epsilon_unocc(scf, scf.W, scf.Z, **scf._precomputed)
     # Merge the occupied and unoccupied energies
     for ik in range(scf.kpts.Nk):
         for spin in range(scf.atoms.occ.Nspin):

eminus/gth.py
@@ -118,8 +118,8 @@ def init_gth_nonloc(atoms, gth):
     for ik in range(atoms.kpts.Nk):
         betaNL_ik = np.empty((len(atoms.Gk2c[ik]), NbetaNL), dtype=complex)
         ibeta = 0
-        g = atoms.G[atoms.active[ik]] + atoms.kpts.k[ik]
-        Gm = np.sqrt(atoms.Gk2c[ik])
+        gk = atoms.G[atoms.active[ik]] + atoms.kpts.k[ik]
+        Gkm = np.sqrt(atoms.Gk2c[ik])
         for ia in range(atoms.Natoms):
             # It is important to transform the structure factor to make both notations compatible
             Sf = atoms.Idag(atoms.J(atoms.Sf[ia], ik), ik)
@@ -127,8 +127,8 @@ def init_gth_nonloc(atoms, gth):
             for l in range(psp['lmax']):
                 for m in range(-l, l + 1):
                     for iprj in range(psp['Nproj_l'][l]):
-                        betaNL_ik[:, ibeta] = (-1j)**l * Ylm_real(l, m, g) * \
-                            eval_proj_G(psp, l, iprj + 1, Gm, atoms.Omega) * Sf
+                        betaNL_ik[:, ibeta] = (-1j)**l * Ylm_real(l, m, gk) * \
+                            eval_proj_G(psp, l, iprj + 1, Gkm, atoms.Omega) * Sf
                         ibeta += 1
         betaNL.append(betaNL_ik)
     return NbetaNL, prj2beta, betaNL

eminus/tools.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.linalg import norm
 from scipy.optimize import minimize_scalar
 
-from .dft import get_epsilon
+from .dft import get_epsilon, get_epsilon_unocc
 from .gga import get_grad_field, get_tau
 from .logger import log
 from .utils import handle_k_gracefully
@@ -421,7 +421,7 @@ def get_Efermi(scf):
         log.warning('The SCF object has no unoccupied energies, return the maximum energy instead.')
         return np.max(e_occ)
 
-    e_unocc = get_epsilon(scf, scf.Z, **scf._precomputed)
+    e_unocc = get_epsilon_unocc(scf, scf.W, scf.Z, **scf._precomputed)
     return np.max(e_occ) + (np.min(e_unocc) - np.max(e_occ)) / 2
 
 
@@ -440,7 +440,7 @@ def get_bandgap(scf):
         log.warning("The SCF object has no unoccupied energies, can't calculate band gap.")
         return 0
 
-    e_unocc = get_epsilon(scf, scf.Z, **scf._precomputed)
+    e_unocc = get_epsilon_unocc(scf, scf.W, scf.Z, **scf._precomputed)
     return np.min(e_unocc) - np.max(e_occ)
 
 