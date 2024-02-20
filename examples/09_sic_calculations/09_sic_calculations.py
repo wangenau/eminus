cc974e8b74dc1b48e4bdc5f2d5c2cf5455146680
Wanja Schulze <wangenau@protonmail.com>
2024-02-20 13:11:06 +0100

    examples: Adjust SIC example

--- examples/09_sic_calculations/09_sic_calculations.py
+++ examples/09_sic_calculations/09_sic_calculations.py
@@ -3,9 +3,9 @@ from eminus.energies import get_Esic
 from eminus.orbitals import FLO
 
 # # Start with a DFT calculation for neon including a SIC calculation
-atoms = Atoms('Ne', [0, 0, 0], ecut=10)
+atoms = Atoms('Be', [0, 0, 0], ecut=10)
 scf = SCF(atoms, sic=True)
-scf.run()
+scf.run(cgform=3)
 
 # # Generate Kohn-Sham and Fermi-Loewdin orbitals
 flo = FLO(scf)
@@ -15,7 +15,7 @@ print(f'\nKSO-SIC energy = {scf.energies.Esic} Eh')
 
 # # The quality of the FLO-SIC energy will vary with the FOD guess
 # # The one-shot FLO-SIC energy should be lower than the KSO-SIC one
-esic = get_Esic(scf, atoms.J(flo, full=False))
+esic = get_Esic(scf, atoms.J(flo))
 print(f'FLO-SIC energy = {esic} Eh')
 # # The SIC energy will also be saved in the `SCF` object
 print(f'\nAll energies:\n{scf.energies}')
