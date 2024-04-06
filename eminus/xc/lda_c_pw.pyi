bc8845e8cc3c9ba917f9fd5d12e6a3b51cacb628
Wanja Schulze <wangenau@protonmail.com>
2024-04-06 10:38:31 +0200

    typing: Simplify type in pw xc

--- eminus/xc/lda_c_pw.pyi
+++ eminus/xc/lda_c_pw.pyi
@@ -1,10 +1,11 @@
 # SPDX-FileCopyrightText: 2021 The eminus developers
 # SPDX-License-Identifier: Apache-2.0
-from collections.abc import Sequence
 from typing import Any
 
 from numpy import float64
 from numpy.typing import NDArray
+
+from ..typing import Array1D
 
 def lda_c_pw(
     n: NDArray[float64],
@@ -19,7 +20,7 @@ def lda_c_pw(
 def lda_c_pw_spin(
     n: NDArray[float64],
     zeta: NDArray[float64],
-    A: Sequence[float] = ...,
+    A: Array1D = ...,
     fzeta0: float = ...,
     **kwargs: Any,
 ) -> tuple[NDArray[float64], NDArray[float64], None]: ...
