0731ba1ee846f1bc5c80a18052c874d44cde486f
Wanja Schulze <wangenau@protonmail.com>
2024-08-19 11:41:22 +0200

    mypy: Exclude examples properly, seems to work now

--- examples/13_geometry_optimization/13_geometry_optimization.py
+++ examples/13_geometry_optimization/13_geometry_optimization.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: 2021 The eminus developers
 # SPDX-License-Identifier: Apache-2.0
-# type: ignore
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import minimize_scalar
--- examples/16_fod_optimization/16_fod_optimization.py
+++ examples/16_fod_optimization/16_fod_optimization.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: 2021 The eminus developers
 # SPDX-License-Identifier: Apache-2.0
-# type: ignore
 import numpy as np
 from scipy.optimize import minimize
 
