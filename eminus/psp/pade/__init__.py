301c252fc693cb78b97aa33609e96aa7a23ec655
Wanja Schulze <wangenau@protonmail.com>
2024-10-04 14:45:57 +0200

    psp: Make updater work with latest python versions

--- eminus/psp/pade/__init__.py
+++ eminus/psp/pade/__init__.py
@@ -11,18 +11,22 @@ if __name__ == "__main__":
     import urllib.request
     import zipfile
 
-    with importlib.resources.path("eminus.psp", "pade") as psp_path:
-        file = "master.zip"
-        # Download files
-        url = f"https://github.com/cp2k/cp2k-data/archive/refs/heads/{file}"
-        urllib.request.urlretrieve(url, file)  # noqa: S310
-        # Unpack files
-        with zipfile.ZipFile(file, "r") as fzip:
-            fzip.extractall()
-        # Move files
-        pade_path = psp_path.joinpath("cp2k-data-master/potentials/Goedecker/cp2k/pade")
-        for f in pade_path.iterdir():
-            shutil.move(str(f), psp_path.joinpath(f.name))
-        # Cleanup
-        psp_path.joinpath(file).unlink()
-        shutil.rmtree("cp2k-data-master")
+    try:
+        psp_path = importlib.resources.files("eminus.psp.pade")
+    except AttributeError:
+        with importlib.resources.path("eminus.psp", "pade") as p:
+            psp_path = p
+    file = "master.zip"
+    # Download files
+    url = f"https://github.com/cp2k/cp2k-data/archive/refs/heads/{file}"
+    urllib.request.urlretrieve(url, file)  # noqa: S310
+    # Unpack files
+    with zipfile.ZipFile(file, "r") as fzip:
+        fzip.extractall()
+    # Move files
+    pade_path = psp_path.joinpath("cp2k-data-master/potentials/Goedecker/cp2k/pade")
+    for f in pade_path.iterdir():
+        shutil.move(str(f), psp_path.joinpath(f.name))
+    # Cleanup
+    psp_path.joinpath(file).unlink()
+    shutil.rmtree("cp2k-data-master")
