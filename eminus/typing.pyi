c20250aa788408363801a1c54d26b68760df7885
Wanja Schulze <wangenau@protonmail.com>
2024-10-14 13:45:12 +0200

    Reapply "typing: Use type over TypeAlias"
    This reverts commit 9ff9606fb1e112747914a4d3030139411dd99a53.
    Now works with mypy 1.12

--- eminus/typing.pyi
+++ eminus/typing.pyi
@@ -1,14 +1,13 @@
 # SPDX-FileCopyrightText: 2021 The eminus developers
 # SPDX-License-Identifier: Apache-2.0
 from collections.abc import Sequence
-from typing import TypeAlias
 
 from numpy import float64, int64
 from numpy.typing import NDArray
 
-Array1D: TypeAlias = Sequence[float] | NDArray[float64]
-Array2D: TypeAlias = Sequence[Sequence[float]] | Sequence[NDArray[float64]] | NDArray[float64]
-Array3D: TypeAlias = (
+type Array1D = Sequence[float] | NDArray[float64]
+type Array2D = Sequence[Sequence[float]] | Sequence[NDArray[float64]] | NDArray[float64]
+type Array3D = (
     Sequence[Sequence[Sequence[float]]] | Sequence[Sequence[NDArray[float64]]] | NDArray[float64]
 )
-IntArray: TypeAlias = Sequence[int] | NDArray[int64]
+type IntArray = Sequence[int] | NDArray[int64]
