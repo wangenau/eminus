fc72ccccadd741956a0e86a232566c5847dc18f6
Wanja Schulze <wangenau@protonmail.com>
2024-12-17 15:17:46 +0100

    Add proper type hints

--- eminus/xc/lda_xc_gdsmfb.py
+++ eminus/xc/lda_xc_gdsmfb.py
@@ -141,13 +141,13 @@ class Coefficients:
     """
 
     theta: float
-    a0 = 0.610887
-    a1 = 0.75
-    a2 = 3.04363
-    a3 = -0.09227
-    a4 = 1.7035
-    a5 = 8.31051
-    a6 = 5.1105
+    a0: float = 0.610887
+    a1: float = 0.75
+    a2: float = 3.04363
+    a3: float = -0.09227
+    a4: float = 1.7035
+    a5: float = 8.31051
+    a6: float = 5.1105
 
     @property
     def b5(self):
@@ -245,23 +245,23 @@ class Zeta0Coeffs(Coefficients):
     Reference: Phys. Rev. Lett. 119, 135001.
     """
 
-    omega = 1
-    b1 = 0.3436902
-    b2 = 7.82159531356
-    b3 = 0.300483986662
-    b4 = 15.8443467125
-    c1 = 0.8759442
-    c2 = -0.230130843551
-    d1 = 0.72700876
-    d2 = 2.38264734144
-    d3 = 0.30221237251
-    d4 = 4.39347718395
-    d5 = 0.729951339845
-    e1 = 0.25388214
-    e2 = 0.815795138599
-    e3 = 0.0646844410481
-    e4 = 15.0984620477
-    e5 = 0.230761357474
+    omega: float = 1
+    b1: float = 0.3436902
+    b2: float = 7.82159531356
+    b3: float = 0.300483986662
+    b4: float = 15.8443467125
+    c1: float = 0.8759442
+    c2: float = -0.230130843551
+    d1: float = 0.72700876
+    d2: float = 2.38264734144
+    d3: float = 0.30221237251
+    d4: float = 4.39347718395
+    d5: float = 0.729951339845
+    e1: float = 0.25388214
+    e2: float = 0.815795138599
+    e3: float = 0.0646844410481
+    e4: float = 15.0984620477
+    e5: float = 0.230761357474
 
 
 @dataclasses.dataclass
@@ -271,23 +271,23 @@ class Zeta1Coeffs(Coefficients):
     Reference: Phys. Rev. Lett. 119, 135001.
     """
 
-    omega = 2 ** (1 / 3)
-    b1 = 0.84987704
-    b2 = 3.04033012073
-    b3 = 0.0775730131248
-    b4 = 7.57703592489
-    c1 = 0.91126873
-    c2 = -0.0307957123308
-    d1 = 1.48658718
-    d2 = 4.92684905511
-    d3 = 0.0849387225179
-    d4 = 8.3269821188
-    d5 = 0.218864952126
-    e1 = 0.27454097
-    e2 = 0.400994856555
-    e3 = 2.88773194962
-    e4 = 6.33499237092
-    e5 = 24.823008753
+    omega: float = 2 ** (1 / 3)
+    b1: float = 0.84987704
+    b2: float = 3.04033012073
+    b3: float = 0.0775730131248
+    b4: float = 7.57703592489
+    c1: float = 0.91126873
+    c2: float = -0.0307957123308
+    d1: float = 1.48658718
+    d2: float = 4.92684905511
+    d3: float = 0.0849387225179
+    d4: float = 8.3269821188
+    d5: float = 0.218864952126
+    e1: float = 0.27454097
+    e2: float = 0.400994856555
+    e3: float = 2.88773194962
+    e4: float = 6.33499237092
+    e5: float = 24.823008753
 
 
 @dataclasses.dataclass
@@ -298,10 +298,10 @@ class PhiParams:
     """
 
     # Sign of parameters is different to the supplemental material
-    h1 = 3.18747258
-    h2 = 7.74662802
-    lambda1 = 1.85909536
-    lambda2 = 0
+    h1: float = 3.18747258
+    h2: float = 7.74662802
+    lambda1: float = 1.85909536
+    lambda2: float = 0
 
 
 # ### Pade approximation and derivative ###
