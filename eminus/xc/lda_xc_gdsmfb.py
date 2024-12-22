88ce1a5bd3a6f2d94427ac16982562aad622a588
Wanja Schulze <wangenau@protonmail.com>
2024-12-22 21:37:50 +0100

    xc/gdsmfb: Improve _get_fxc performance and readability

--- eminus/xc/lda_xc_gdsmfb.py
+++ eminus/xc/lda_xc_gdsmfb.py
@@ -413,31 +413,27 @@ def _get_fxc_zeta(rs, p):
 
     Reference: Phys. Rev. Lett. 119, 135001.
     """
-    num = p.omega * p.a + np.sqrt(rs) * p.b + rs * p.c
-    denom = 1 + np.sqrt(rs) * p.d + rs * p.e
+    num = p.a * p.omega + p.b * np.sqrt(rs) + p.c * rs
+    denom = 1 + p.d * np.sqrt(rs) + p.e * rs
     return -1 / rs * num / denom
 
 
 def _get_dfxc_zetadrs(rs, p):
     """Calculate dfxc_zeta / drs."""
-    tmp1 = (-p.b / (2 * np.sqrt(rs)) - p.c) / (rs * (p.d * np.sqrt(rs) + p.e * rs + 1))
-    tmp2 = (-p.d / (2 * np.sqrt(rs)) - p.e) * (-p.a * p.omega - p.b * np.sqrt(rs) - p.c * rs)
-    tmp3 = rs * (p.d * np.sqrt(rs) + p.e * rs + 1) ** 2
-    tmp4 = (-p.a * p.omega - p.b * np.sqrt(rs) - p.c * rs) / (
-        rs**2 * (p.d * np.sqrt(rs) + p.e * rs + 1)
-    )
-    return tmp1 + tmp2 / tmp3 - tmp4
+    num = p.a * p.omega + p.b * np.sqrt(rs) + p.c * rs
+    denom = 1 + p.d * np.sqrt(rs) + p.e * rs
+    tmp1 = (p.d / (2 * np.sqrt(rs)) + p.e) * num / denom**2
+    tmp2 = (p.b / (2 * np.sqrt(rs)) + p.c) / denom
+    return (tmp1 - tmp2) / rs + num / denom / rs**2
 
 
 def _get_dfxc_zetadtheta(rs, p):
     """Calculate dfxc_zeta / dzeta."""
-    tmp1 = (-np.sqrt(rs) * p.dddtheta - rs * p.dedtheta) * (
-        -p.omega * p.a - p.b * np.sqrt(rs) - p.c * rs
-    )
-    tmp2 = (p.d * np.sqrt(rs) + p.e * rs + 1) ** 2 * rs
-    tmp3 = -p.omega * p.dadtheta - np.sqrt(rs) * p.dbdtheta - rs * p.dcdtheta
-    tmp4 = (p.d * np.sqrt(rs) + p.e * rs + 1) * rs
-    return tmp1 / tmp2 + tmp3 / tmp4
+    num = p.a * p.omega + p.b * np.sqrt(rs) + p.c * rs
+    denom = 1 + p.d * np.sqrt(rs) + p.e * rs
+    tmp1 = (p.dddtheta * np.sqrt(rs) + p.dedtheta * rs) * num / denom**2
+    tmp2 = (p.dadtheta * p.omega + p.dbdtheta * np.sqrt(rs) + p.dcdtheta * rs) / denom
+    return (tmp1 - tmp2) / rs
 
 
 # ### phi and derivatives ###
@@ -455,12 +451,12 @@ def _get_phi(rs, theta, zeta, phi_params):
 def _get_dphidrs(rs, theta, zeta, phi_params):
     """Calculate dphi / drs."""
     alpha = _get_alpha(rs, theta, phi_params)
+    dalphadrs = _get_dalphadrs(rs, theta, phi_params)
     tmp1 = (1 - zeta) ** alpha * np.log(1 - zeta, out=np.zeros_like(zeta), where=1 - zeta > 0)
     tmp2 = (1 + zeta) ** alpha * np.log(1 + zeta)
     duv = (tmp1 + tmp2) * (2**alpha - 2)
     udv = ((1 - zeta) ** alpha + (1 + zeta) ** alpha - 2) * 2**alpha * np.log(2)
     vv = (2**alpha - 2) ** 2
-    dalphadrs = _get_dalphadrs(rs, theta, phi_params)
     return (duv - udv) * dalphadrs / vv
 
 
@@ -473,7 +469,6 @@ def _get_dphidtheta(rs, theta, zeta, phi_params):
     duv = (tmp1 + tmp2) * (2**alpha - 2)
     udv = ((1 - zeta) ** alpha + (1 + zeta) ** alpha - 2) * 2**alpha * np.log(2)
     vv = (2**alpha - 2) ** 2
-    dalphadtheta = _get_dalphadtheta(rs, theta, phi_params)
     return (duv - udv) * dalphadtheta / vv
 
 
