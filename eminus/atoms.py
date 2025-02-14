atoms: Fix calculation of sampling points, also fix type hint

eminus/atoms.py
@@ -138,7 +138,7 @@ class Atoms(BaseObject):
     def ecut(self, value):
         self._ecut = value
         # Calculate the sampling from the cut-off energy
-        s = np.int64(norm(self.a, axis=0) / cutoff2gridspacing(value))
+        s = np.int64(norm(self.a, axis=1) / cutoff2gridspacing(value))
         # Multiply by two and add one to match PWDFT.jl
         s = 2 * s + 1
         # Calculate a fast length to optimize the FFT calculations