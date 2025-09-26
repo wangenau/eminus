543ea60302a7a28076b5fd301ebe6af8dd0608f1
Wanja Schulze <wangenau@protonmail.com>
2025-09-26 17:04:23 +0200

    tests: Increase timeout for notebook tests

--- tests/extras/test_gui.py
+++ tests/extras/test_gui.py
@@ -33,7 +33,7 @@ def test_gui(name):
     os.environ["PYTEST_TEST_DIR"] = str(file_path)
     with open(f"{file_path}/{name}.ipynb", encoding="utf-8") as fh:
         nb = read(fh, as_version=4)  # type: ignore[no-untyped-call]
-        ep = ExecutePreprocessor(timeout=60)  # type: ignore[no-untyped-call]
+        ep = ExecutePreprocessor(timeout=120)  # type: ignore[no-untyped-call]
         assert ep.preprocess(nb) is not None
 
 
